bc2757d172f09aaf639909974a050b737c27b1b6
zuronghun <128782538+zuronghun@users.noreply.github.com>
2025-09-14 22:13:04 +0800

    feat: update HYX resume

---
+++ variables.js
@@ -0,0 +1,5 @@
+// Declare global variables
+const name = "Hun Yu Xuan";
+var headerProfileImgPath = "img/yx-profile.png"; // png w/o bg
+var sitebarProfileImgPath = "img/yx-profile.jpg"; // jpg w bg
+var profileImgPath = "img/yx-profile.jpg"; // jpg w bg
