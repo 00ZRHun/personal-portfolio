8f8c8e876d0350fb5c53448528cec95a30604738
zuronghun <128782538+zuronghun@users.noreply.github.com>
2025-09-15 00:27:12 +0800

    chore: fix wording 2

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
 // Declare global variables
-const name = "Hun Yu Xuan";
+const name = "Yu Xuan";
 var headerProfileImgPath = "img/yx-profile.png"; // png w/o bg
 var sitebarProfileImgPath = "img/yx-profile.jpg"; // jpg w bg
 var profileImgPath = "img/yx-profile.jpg"; // jpg w bg
